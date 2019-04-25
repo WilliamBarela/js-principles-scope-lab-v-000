53fe133cdca7e05e927d1e051cabea88d1295107
William Barela <william.barela.dev@gmail.com>
2019-04-25 03:20:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+const leastFavoriteCustomer = "alice";
+
+function upperCaseCustomerName(){
+  customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+  var bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(customer){
+  bestCustomer = customer;
+}
+
+function changeLeastFavoriteCustomer(customer){
+  leastFavoriteCustomer = customer;
+}
